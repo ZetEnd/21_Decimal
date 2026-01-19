52b35cb2dacdde703535e91034ffe00f94c90b02
unknown <p.timoshev@gmail.com>
2026-01-19 14:31:35 +0300

    working at dloat decimal

--- src/Convertors/s21_from_float_to_decimal.c
+++ src/Convertors/s21_from_float_to_decimal.c
@@ -100,7 +100,7 @@ int s21_from_float_to_decimal(float src, s21_decimal* dst){
 
     int sign;
     // устанавливаем знак числа десималь
-    if(src < 0.0f) sign = 1
+    if(src < 0.0f) sign = 1;
     else sign = 0;
 
     double abs_src = fabs((double)src);
@@ -112,7 +112,61 @@ int s21_from_float_to_decimal(float src, s21_decimal* dst){
     int exp10 = (int)floor(log10(abs_src));
     double pow_neg = pow(10.0, -exp10);
     double norm = abs_src * pow_neg;
+
+    // проверяем на нормированное значение
+    if(norm >= 10.0) {
+      norm /= 10.0;
+      exp10 +=1;
+    }
+    if(norm < 1.0){
+
+      norm *= 10.0;
+      exp10 -= 1;
+    }
+
+    // извлечение 7ми цифр
+    const int digits = 7;
+    double scaled = norm * pow(10.0, digits - 1);
+    uint64_t mantissa7 = round_ties_to_even(scaled);
+
+    // проверка на переполнение в коде, 8 цифр
+    if(mantissa7 = 10000000ull){
+      mantissa7 = 1000000ull;
+      exp10 += 1;
+    }
+
+    // рассчитываем значение масштаба числа десималб
+    int scale = (digits - 1) - exp10;
+
+    uint32_t number[3];
+    u96_from_u64(mantissa7, number);
+
+    // помещаем размер масштаба в диапазон от 0 до 28
+    if(scale < 0){
+      int k = -scale;
+      if(u96_mul_pow10(number, k) != 0) return 1;
+
+      scale = 0;
+    }
+
+    while(scale > 28){
+      u96_round_div10(number);
+      scale -= 1;
+    }
+
+    // устанавливаем мантиссу масштаб и знак десималь числа
+    u96_to_dec(number, dst);
+    s21_set_scale(dst, scale);
+    s21_set_sign(dst, sign);
+
+    // если мантисса нулевая - делаем полное обнуление числа деималь
+    if(u96_is_zero(number)){
+      s21_reset_value(dst);
+    }
+
   }
+
+  return 0;
 
   
 }
